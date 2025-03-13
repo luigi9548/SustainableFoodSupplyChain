8f4b4c507b70690ce937a1027e8e68734b3499cb
elena1812 <elenapagano02@gmail.com>
2025-03-13 19:56:27 +0100

    Correzione classe base dei models

--- off_chain/models/model_base.py
+++ off_chain/models/model_base.py
@@ -4,11 +4,6 @@ from config import config
 class Model:
     """Base model to be extended for implementing other models."""
     
-    def __init__(self):
-        """Constructor that initializes the database connection."""
-        self.conn = sqlite3.connect(config["db_path"])
-        self.cur = self.conn.cursor()
-    
     def save(self):
         """Virtual method to save the model. Must be implemented by subclasses."""
         raise NotImplementedError("Subclasses must implement this method.")
@@ -16,14 +11,5 @@ class Model:
     def delete(self):
         """Virtual method to delete the model. Must be implemented by subclasses."""
         raise NotImplementedError("Subclasses must implement this method.")
-    
-    def close_connection(self):
-        """Closes the database connection explicitly."""
-        if self.conn:
-            self.conn.close()
-            self.conn = None
 
-    def __del__(self):
-        """Destructor that closes the database connection."""
-        self.close_connection()
 
